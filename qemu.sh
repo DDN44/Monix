3bb0779206ec5781e67fb17a2162051c064d24c8
DDN44 <spaghettimans1234@gmail.com>
2025-08-06 13:52:44 -0500

    qemu

--- qemu.sh
+++ qemu.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-<<<<<<< HEAD
 qemu-system-i386 -m 512M -boot menu=on -hda ./drive2 -cdrom ./Monix.iso -serial stdio
-=======
-qemu-system-i386 -m 512M -boot menu=on -hda ./drive2 -cdrom ./Monix.iso -serial stdio
->>>>>>> 4b18a3a (Many improvements)
 
 ##qemu-system-i386 -m 512m -boot menu=on -hda ./testdisk.img -cdrom ./Monix.iso -serial stdio
